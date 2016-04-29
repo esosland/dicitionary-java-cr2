cd30caf3756b0c1e13d756a218d7093e5f08d3b4
Elissa Sosland <esosland@gmail.com>
2016-04-29 08:36:57 -0700

    add first passing test to create new instance of Definition class

---
+++ src/test/java/DefinitionTest.java
@@ -0,0 +1,12 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class DefinitionTest {
+
+  @Test
+  public void Definition_instantiatesCorrectly_true() {
+    Definition myDefinition = new Definition("Delightful");
+    assertEquals(true, myDefinition instanceof Definition);
+  }
+
+}

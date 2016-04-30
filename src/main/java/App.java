18989f0d8489aa9ae15b130f61a582e96b2225c0
Elissa Sosland <esosland@gmail.com>
2016-04-29 22:08:19 -0700

    add integration test and build out front end word creation

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,7 @@
-import java.util.Map;
 import java.util.HashMap;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
-import java.util.ArrayList;
 
 public class App {
   public static void main(String[] args) {
@@ -11,50 +9,87 @@ public class App {
     String layout = "templates/layout.vtl";
 
     get("/", (request, response) -> {
-      Map<String, Object> model = new HashMap<String, Object>();
+      HashMap<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    get("definitions/new", (request, response) -> {
+    get("/words/new", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
-      model.put("template", "templates/definition-form.vtl");
+      model.put("template", "templates/word-form.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    get("/definitions", (request, response) -> {
+    post("/words", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
-      model.put("definitions", Definition.all());
-      model.put("template", "templates/definitions.vtl");
-      return new ModelAndView(model, layout);
-    }, new VelocityTemplateEngine());
-
-    post("/definitions", (request,response) -> {
-      HashMap<String, Object> model = new HashMap<String, Object>();
-      String firstDefinition = request.queryParams("firstDefinition");
-      Definition newDefinition = new Definition(firstDefinition);
-      model.put("template", "templates/success.vtl");
-      return new ModelAndView(model, layout);
-    }, new VelocityTemplateEngine());
-
-    get("/definitions/:id", (request, response) -> {
-      HashMap<String, Object> model = new HashMap<String, Object>();
-      Definition definition = Definition.find(Integer.parseInt(request.params(":id")));
-      model.put("definition", definition);
-      model.put("template", "templates/definition.vtl");
-      return new ModelAndView(model, layout);
-    }, new VelocityTemplateEngine());
-
-    get("/definitions/:id", (request, response) -> {
-      HashMap<String, Object> model = new HashMap<String, Object>();
-      Definition definition = Definition.find(Integer.parseInt(request.params(":id")));
-      model.put("definition", definition);
-      model.put("template", "templates/definition.vtl");
+      String name = request.queryParams("name");
+      Word newWord = new Word(name);
+      model.put("template", "templates/word-success.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
   }
 }
+
+
+
+// import java.util.Map;
+// import java.util.HashMap;
+// import spark.ModelAndView;
+// import spark.template.velocity.VelocityTemplateEngine;
+// import static spark.Spark.*;
+// import java.util.ArrayList;
+//
+// public class App {
+//   public static void main(String[] args) {
+//     staticFileLocation("/public");
+//     String layout = "templates/layout.vtl";
+//
+//     get("/", (request, response) -> {
+//       Map<String, Object> model = new HashMap<String, Object>();
+//       model.put("template", "templates/index.vtl");
+//       return new ModelAndView(model, layout);
+//     }, new VelocityTemplateEngine());
+//
+//     get("definitions/new", (request, response) -> {
+//       HashMap<String, Object> model = new HashMap<String, Object>();
+//       model.put("template", "templates/definition-form.vtl");
+//       return new ModelAndView(model, layout);
+//     }, new VelocityTemplateEngine());
+//
+//     get("/definitions", (request, response) -> {
+//       HashMap<String, Object> model = new HashMap<String, Object>();
+//       model.put("definitions", Definition.all());
+//       model.put("template", "templates/definitions.vtl");
+//       return new ModelAndView(model, layout);
+//     }, new VelocityTemplateEngine());
+//
+//     post("/definitions", (request,response) -> {
+//       HashMap<String, Object> model = new HashMap<String, Object>();
+//       String firstDefinition = request.queryParams("firstDefinition");
+//       Definition newDefinition = new Definition(firstDefinition);
+//       model.put("template", "templates/success.vtl");
+//       return new ModelAndView(model, layout);
+//     }, new VelocityTemplateEngine());
+//
+//     get("/definitions/:id", (request, response) -> {
+//       HashMap<String, Object> model = new HashMap<String, Object>();
+//       Definition definition = Definition.find(Integer.parseInt(request.params(":id")));
+//       model.put("definition", definition);
+//       model.put("template", "templates/definition.vtl");
+//       return new ModelAndView(model, layout);
+//     }, new VelocityTemplateEngine());
+//
+//     get("/definitions/:id", (request, response) -> {
+//       HashMap<String, Object> model = new HashMap<String, Object>();
+//       Definition definition = Definition.find(Integer.parseInt(request.params(":id")));
+//       model.put("definition", definition);
+//       model.put("template", "templates/definition.vtl");
+//       return new ModelAndView(model, layout);
+//     }, new VelocityTemplateEngine());
+//
+//   }
+// }
 
 
 //     post("/definitions", (request, response) -> {
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -21,53 +21,94 @@ public class AppTest extends FluentTest {
   public void rootTest() {
     goTo("http://localhost:4567/");
     assertThat(pageSource()).contains("Dictionary!");
+    assertThat(pageSource()).contains("View Word List");
+    assertThat(pageSource()).contains("Add a New Word");
   }
 
   @Test
-  public void DefinitionIsCreatedTest() {
+  public void wordIsCreatedTest() {
     goTo("http://localhost:4567/");
-    click("a", withText("Add a new definition"));
-    fill("#firstDefinition").with("Delightful");
+    click("a", withText("Add a New Word"));
+    fill("#name").with("Frabjous");
     submit(".btn");
-    assertThat(pageSource()).contains("Your definition has been saved.");
-  }
-
-  @Test
-  public void definitionIsDisplayedTest() {
-    goTo("http://localhost:4567/definitions/new");
-    fill("#firstDefinition").with("Delightful");
-    submit(".btn");
-    click("a", withText("View definitions"));
-    assertThat(pageSource()).contains("Delightful");
-  }
-
-  @Test
-  public void multipleDefinitionsAreDisplayedTest() {
-    goTo("http://localhost:4567/definitions/new");
-    fill("#firstDefinition").with("Delightful");
-    submit(".btn");
-    goTo("http://localhost:4567/definitions/new");
-    fill("#firstDefinition").with("Wonderful");
-    submit(".btn");
-    click("a", withText("View definitions"));
-    assertThat(pageSource()).contains("Delightful");
-    assertThat(pageSource()).contains("Wonderful");
-  }
-
-  @Test
-  public void definitionShowPageDisplaysFirstDefinition() {
-    goTo("http://localhost:4567/definitions/new");
-    fill("#firstDefinition").with("Splendid");
-    submit(".btn");
-    click("a", withText("View definitions"));
-    click("a", withText("Splendid"));
-    assertThat(pageSource()).contains("Splendid");
-  }
-
-  @Test
-  public void definitionNotFoundMessageShown() {
-    goTo("http://localhost:4567/definitions/999");
-    assertThat(pageSource()).contains("Definition not found");
+    assertThat(pageSource()).contains("Your word has been saved.");
   }
 
 }
+
+
+
+
+// import org.fluentlenium.adapter.FluentTest;
+// import org.junit.ClassRule;
+// import org.junit.Test;
+// import org.openqa.selenium.WebDriver;
+// import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+// import static org.assertj.core.api.Assertions.assertThat;
+// import static org.fluentlenium.core.filter.FilterConstructor.*;
+//
+// public class AppTest extends FluentTest {
+//   public WebDriver webDriver = new HtmlUnitDriver();
+//
+//   @Override
+//   public WebDriver getDefaultDriver() {
+//     return webDriver;
+//   }
+//
+//   @ClassRule
+//   public static ServerRule server = new ServerRule();
+//
+//   @Test
+//   public void rootTest() {
+//     goTo("http://localhost:4567/");
+//     assertThat(pageSource()).contains("Dictionary!");
+//   }
+//
+//   @Test
+//   public void DefinitionIsCreatedTest() {
+//     goTo("http://localhost:4567/");
+//     click("a", withText("Add a new definition"));
+//     fill("#firstDefinition").with("Delightful");
+//     submit(".btn");
+//     assertThat(pageSource()).contains("Your definition has been saved.");
+//   }
+//
+//   @Test
+//   public void definitionIsDisplayedTest() {
+//     goTo("http://localhost:4567/definitions/new");
+//     fill("#firstDefinition").with("Delightful");
+//     submit(".btn");
+//     click("a", withText("View definitions"));
+//     assertThat(pageSource()).contains("Delightful");
+//   }
+//
+//   @Test
+//   public void multipleDefinitionsAreDisplayedTest() {
+//     goTo("http://localhost:4567/definitions/new");
+//     fill("#firstDefinition").with("Delightful");
+//     submit(".btn");
+//     goTo("http://localhost:4567/definitions/new");
+//     fill("#firstDefinition").with("Wonderful");
+//     submit(".btn");
+//     click("a", withText("View definitions"));
+//     assertThat(pageSource()).contains("Delightful");
+//     assertThat(pageSource()).contains("Wonderful");
+//   }
+//
+//   @Test
+//   public void definitionShowPageDisplaysFirstDefinition() {
+//     goTo("http://localhost:4567/definitions/new");
+//     fill("#firstDefinition").with("Splendid");
+//     submit(".btn");
+//     click("a", withText("View definitions"));
+//     click("a", withText("Splendid"));
+//     assertThat(pageSource()).contains("Splendid");
+//   }
+//
+//   @Test
+//   public void definitionNotFoundMessageShown() {
+//     goTo("http://localhost:4567/definitions/999");
+//     assertThat(pageSource()).contains("Definition not found");
+//   }
+//
+// }
